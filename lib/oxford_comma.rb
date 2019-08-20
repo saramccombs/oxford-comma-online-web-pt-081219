5714221c8f09520a3b0daef7583a6df310fa071d
Sara McCombs <thesaramccombs@gmail.com>
2019-08-20 02:05:57 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,20 @@
-def oxford_comma(array)
+# method takes in an array of string elements and converts them into a string using the oxford comma
 
+def oxford_comma(array)
+  string = ""
+  
+  if array.length == 1 
+    string = array[0]
+  elsif array.length == 2 
+    string = "#{array[0]} and #{array[1]}"
+  else
+    array.each do |element|
+      if element != array[-1]
+        string += "#{element}, "
+      else
+        string += "and #{element}"
+      end
+    end
+  end
+  string
 end
